5348843358d0cc400e3a447e849df411558b82ba
jorgevz <jorgev2132@gmail.com>
2020-11-23 01:29:50 -0400

    variable name change

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-
+// In this function I am performing the whole calculation
 function bmiOperation() {
+    //height input variable
     let height = document.getElementById('height').value;
-
+    //weight input variable
     let weight = document.getElementById('weight').value;
 
     let constant = 703;
 
-    let realw = Math.pow(height, 2);
 
-    let result = constant * weight/realw;
+    let realWeight = Math.pow(height, 2);
+
+    let result = constant * weight/realWeight;
 
     console.log(result)
 
