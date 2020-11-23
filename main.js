fe82b410e14a4afb00416a207bb12d4d93372cfa
jorgevz <jorgev2132@gmail.com>
2020-11-23 00:51:41 -0400

    javascript functioned built

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function mySubmit() {
-    window.location.href("https://www.shein.com/?&ici=www_tab01")
+
+function bmiOperation() {
+    let height = document.getElementById('height').value;
+
+    let weight = document.getElementById('weight').value;
+
+    let constant = 703;
+
+    let result = constant * weight/height;
+
+    console.log(result)
+
 }
 
-// https://www.google.com/search?q=how+to+calculate+bmi&sxsrf=ALeKk001kq13-EeMvufCHhIx9Z-mmV0Z4Q:1606024147966&tbm=isch&source=iu&ictx=1&fir=CWxFwnP0opeczM%252CX9ate5_0PkRSHM%252C_&vet=1&usg=AI4_-kTlxMwpeaQ1IYuAmVekXFOmU82oog&sa=X&ved=2ahUKEwi-8duWupXtAhUlSTABHVvrD-MQ9QF6BAgJEDc&biw=1440&bih=821#imgrc=CWxFwnP0opeczM
