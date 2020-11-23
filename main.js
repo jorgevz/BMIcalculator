0d93768fa7fee764c8bb9c1a5e7ff229da3de199
jorgevz <jorgev2132@gmail.com>
2020-11-22 21:26:34 -0400

    first page adjustments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function mySubmit() {
     window.location.href("https://www.shein.com/?&ici=www_tab01")
 }
+
+// https://www.google.com/search?q=how+to+calculate+bmi&sxsrf=ALeKk001kq13-EeMvufCHhIx9Z-mmV0Z4Q:1606024147966&tbm=isch&source=iu&ictx=1&fir=CWxFwnP0opeczM%252CX9ate5_0PkRSHM%252C_&vet=1&usg=AI4_-kTlxMwpeaQ1IYuAmVekXFOmU82oog&sa=X&ved=2ahUKEwi-8duWupXtAhUlSTABHVvrD-MQ9QF6BAgJEDc&biw=1440&bih=821#imgrc=CWxFwnP0opeczM
